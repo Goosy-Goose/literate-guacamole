052d96abfe851cfc22865bc8f64f474e7558b7ae
winchell459 <johncmorris@gmail.com>
2021-06-07 15:03:16 -0700

    initial commit

---
+++ Assets/Scripts/Balloon.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    private bool popped = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+
+        poppingStart();
+    }
+
+    private void poppingStart()
+    {
+        if (!popped)
+        {
+            GetComponent<Animator>().SetTrigger("Pop");
+            popped = true;
+        }
+        
+    }
+
+    public void PopCompleted()
+    {
+        Destroy(gameObject);
+    }
+    
+
+}
